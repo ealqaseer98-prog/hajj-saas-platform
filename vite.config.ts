d4f5e48cfd248052e6ec87091a47ae3c3998cbdc
Elyas <ealqaseer98@gmail.com>
2026-05-06 11:19:21 +0300

    fix build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
+        warn(warning)
+      }
+    }
+  }
 })
